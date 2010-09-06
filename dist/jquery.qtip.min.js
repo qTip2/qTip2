e63a7f2b4fc5ca8d1c8ebe531218fcf4c7f5c6ca
Craig Michael Thompson <craig@craigsworks.com>
2010-09-06 14:15:53 +0100

    Removed 'div' qualifier from CSS, which was only needed when style.widget class was on by default to prevent overrides, which you probably want anything if you've enabled it!

--- dist/jquery.qtip.min.js
+++ dist/jquery.qtip.min.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Mon Sep 6 13:58:33 2010 +0100
+* Date: Mon Sep 6 14:13:03 2010 +0100
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
