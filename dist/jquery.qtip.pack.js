36afa6bcc857059a3028ae21b8c4dfec4180bef3
Craig Michael Thompson <craig@craigsworks.com>
2010-09-16 19:53:26 +0100

    Removed display:block from .qtip class in favour of offside positioning

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Thu Sep 16 19:04:17 2010 +0100
+* Date: Thu Sep 16 19:48:07 2010 +0100
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
