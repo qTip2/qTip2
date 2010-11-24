7fd522a871e5e9e55265436ebee07ee009b4012b
Craig Michael Thompson <craig@craigsworks.com>
2010-11-24 23:43:38 +0000

    Adjusted shadow class again... booya!

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Wed Nov 24 22:19:30 2010 +0000
+* Date: Wed Nov 24 23:29:43 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
