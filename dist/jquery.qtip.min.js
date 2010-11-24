bdcd348dfd0d771a3df03a1851d8ce3be1dc4fb8
Craig Michael Thompson <craig@craigsworks.com>
2010-11-24 22:19:30 +0000

    More appealing shadows... yessh.

--- dist/jquery.qtip.min.js
+++ dist/jquery.qtip.min.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Wed Nov 24 15:49:09 2010 +0000
+* Date: Wed Nov 24 22:16:44 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
