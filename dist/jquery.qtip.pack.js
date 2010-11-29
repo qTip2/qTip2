7e2712ce3fea8d98544ce6b0d2fecffecf9d443e
Craig Michael Thompson <craig@craigsworks.com>
2010-11-29 20:13:19 +0000

    Moved box-shadow to the wrapper element preventing square corner issues when using rounded class

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Mon Nov 29 17:02:40 2010 +0000
+* Date: Mon Nov 29 17:06:55 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
