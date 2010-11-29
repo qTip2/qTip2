35d451fe997f2335f0f11b382a455481bfea2459
Craig Michael Thompson <craig@craigsworks.com>
2010-11-29 04:03:29 +0000

    Added  new 'tipsy' style to the extra.css stylesheet

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Fri Nov 26 13:15:03 2010 +0000
+* Date: Sun Nov 28 23:14:08 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
