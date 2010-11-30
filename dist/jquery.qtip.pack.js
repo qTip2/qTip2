e77e8fecc333a1bf9ed73731ddf0cdc65248ae93
Craig Michael Thompson <craig@craigsworks.com>
2010-11-30 03:53:34 +0000

    Upped the opacity of tipsy tooltips background

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Tue Nov 30 02:48:10 2010 +0000
+* Date: Tue Nov 30 03:47:08 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
