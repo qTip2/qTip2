7d5ee2b53e5bef6225cda4387f2f6a16102d49d7
Craig Michael Thompson <craig@craigsworks.com>
2010-12-08 18:43:13 +0000

    Added additional themeroller compat class

--- dist/jquery.qtip.min.js
+++ dist/jquery.qtip.min.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Wed Dec 8 18:37:35 2010 +0000
+* Date: Wed Dec 8 18:41:36 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
