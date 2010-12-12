877f0618865993ba7a169986c565053ba7586166
Craig Michael Thompson <craig@craigsworks.com>
2010-12-12 01:43:24 +0000

    Fixed indentation bug from last commit

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Thu Dec 9 16:06:43 2010 +0000
+* Date: Sun Dec 12 00:13:54 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
