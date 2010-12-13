04e4bf6b2d7d8a11c5600d692aec7edc3b20f94f
Craig Michael Thompson <craig@craigsworks.com>
2010-12-13 22:40:29 +0000

    Made the IE drop shadows a bit nicer to look at

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Mon Dec 13 21:36:03 2010 +0000
+* Date: Mon Dec 13 21:52:07 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
