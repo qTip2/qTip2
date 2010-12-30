3f6c09e991be95b07edc6cacd71b244d98c6a780
Craig Michael Thompson <craig@craigsworks.com>
2010-12-30 08:05:55 +0000

    Fixed ui-tooltip-rounded class

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Thu Dec 30 07:47:53 2010 +0000
+* Date: Thu Dec 30 07:55:44 2010 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
