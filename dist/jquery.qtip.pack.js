b68d0fe905418c9c8c9e129aa9eb4adc26f204cb
Craig Michael Thompson <craig@craigsworks.com>
2011-01-12 22:29:27 +0000

    Fixed some CSS bugs and minimized some styles down

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Wed Jan 12 21:13:05 2011 +0000
+* Date: Wed Jan 12 21:55:39 2011 +0000
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
