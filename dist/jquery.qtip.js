54c41ff9290fc89eae4472a0ec80bd5afca8bbce
Craig Michael Thompson <craig@craigsworks.com>
2011-04-04 17:51:01 +0100

    Adjusted iOS detection regex to be more inclusive

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Mon Apr 4 17:42:29 2011 +0100
+* Date: Mon Apr 4 17:47:28 2011 +0100
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
@@ -1552,7 +1552,7 @@ PLUGINS = QTIP.plugins = {
 	 * iOS 4.0 and below scroll fix detection used in offset() function.
 	 */
 	iOS: parseFloat(
-		('' + (/CPU.*OS ([0-9_]{3})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,'4_2'])[1])
+		('' + (/CPU.*OS ([0-9_]{2,3})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,'4_2'])[1])
 		.replace('undefined', '3_2').replace('_','.')
 	) < 4.1,
 	
