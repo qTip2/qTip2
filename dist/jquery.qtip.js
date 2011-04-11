75f8c76783c769bc2fa46b6b5c0fad2cecec1c30
Craig Michael Thompson <craig@craigsworks.com>
2011-04-11 17:44:04 +0100

    Fixed canvas tip positioning issue

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Sat Apr 9 11:50:12 2011 +0100
+* Date: Mon Apr 11 17:12:40 2011 +0100
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
