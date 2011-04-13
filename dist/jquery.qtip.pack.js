b09a56b0788d9803a911aea34c596097b66c9df1
Craig Michael Thompson <craig@craigsworks.com>
2011-04-13 11:43:08 +0100

    Fixed Opera positioning issue taht was due to largenegative offset values in CSS

--- dist/jquery.qtip.pack.js
+++ dist/jquery.qtip.pack.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Tue Apr 12 02:19:46 2011 +0100
+* Date: Tue Apr 12 19:24:46 2011 +0100
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
