f13bdc6dabc8e4d33b9e1a1e26e445e1b83cf1e8
Craig Michael Thompson <craig@craigsworks.com>
2011-04-13 23:56:53 +0100

    Lulz maybe not...

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Wed Apr 13 23:51:41 2011 +0100
+* Date: Wed Apr 13 23:54:54 2011 +0100
 */
 
 "use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
@@ -1601,9 +1601,9 @@ PLUGINS = QTIP.plugins = {
 	 * iOS 3.2 - 4.0 scroll fix detection used in offset() function.
 	 */
 	iOS: parseFloat(
-		('' + (/CPU.*OS ([0-9_]{1,3})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,'4_2'])[1])
+		('' + (/CPU.*OS ([0-9_]{1,3})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
 			.replace('undefined', '3_2').replace('_','.')
-	),
+	) || FALSE,
 	
 	/*
 	 * jQuery-secpfic $.fn overrides 
--- src/core.js
+++ src/core.js
@@ -1559,9 +1559,9 @@ PLUGINS = QTIP.plugins = {
 	 * iOS 3.2 - 4.0 scroll fix detection used in offset() function.
 	 */
 	iOS: parseFloat(
-		('' + (/CPU.*OS ([0-9_]{1,3})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,'4_2'])[1])
+		('' + (/CPU.*OS ([0-9_]{1,3})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
 			.replace('undefined', '3_2').replace('_','.')
-	),
+	) || FALSE,
 	
 	/*
 	 * jQuery-secpfic $.fn overrides 
