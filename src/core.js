8e40228873c8ccf4637f6f78d38594e0f602ee03
Craig Michael Thompson <craig@craigsworks.com>
2011-04-26 14:33:26 +0100

    Few more reposition updates

--- src/core.js
+++ src/core.js
@@ -1033,20 +1033,21 @@ function QTip(target, options, id, attr)
 
 					// Reposition methods
 					left: function(posLeft) {
-						var viewportScroll = viewport.offset.left + viewport.scrollLeft,
+						var isShift = readjust.horizontal === 'shift',
+							viewportScroll = viewport.offset.left + viewport.scrollLeft,
 							myWidth = my.x === 'left' ? elemWidth : my.x === 'right' ? -elemWidth : -elemWidth / 2,
 							atWidth = at.x === 'left' ? targetWidth : at.x === 'right' ? -targetWidth : -targetWidth / 2,
 							tipWidth = readjust.tip.width + readjust.tip.border * 2,
-							tipAdjust = tip && tip.precedance === 'x' ? tipWidth : 0,
+							tipAdjust = tip && tip.precedance === 'x' && !isShift ? tipWidth : 0,
 							overflowLeft = viewportScroll - posLeft - tipAdjust,
 							overflowRight = posLeft + elemWidth - viewport.width - viewportScroll + tipAdjust,
 							offset = myWidth - (my.precedance === 'x' || my.x === my.y ? atWidth : 0),
 							isCenter = my.x === 'center';
 
 						// Optional 'shift' style repositioning
-						if(readjust.horizontal === 'shift') {
+						if(isShift) {
 							tipAdjust = tip && tip.precedance === 'y' ? tipWidth : 0;
-							offset = (my.x === 'left' ? myWidth : -myWidth) - tipAdjust;
+							offset = (my.x === 'left' ? 1 : -1) * myWidth - tipAdjust;
 
 							// Adjust position but keep it within viewport dimensions
 							position.left += overflowLeft > 0 ? overflowLeft : overflowRight > 0 ? -overflowRight : 0;
@@ -1074,20 +1075,21 @@ function QTip(target, options, id, attr)
 						return position.left - posLeft;
 					},
 					top: function(posTop) {
-						var viewportScroll = viewport.offset.top + viewport.scrollTop,
+						var isShift = readjust.vertical === 'shift',
+							viewportScroll = viewport.offset.top + viewport.scrollTop,
 							myHeight = my.y === 'top' ? elemHeight : my.y === 'bottom' ? -elemHeight : -elemHeight / 2,
 							atHeight = at.y === 'top' ? targetHeight : at.y === 'bottom' ? -targetHeight : -targetHeight / 2,
 							tipHeight = readjust.tip.height + readjust.tip.border * 2,
-							tipAdjust = tip && tip.precedance === 'y' ? tipHeight : 0,
+							tipAdjust = tip && tip.precedance === 'y' && !isShift ? tipHeight : 0,
 							overflowTop = viewportScroll - posTop - tipAdjust,
 							overflowBottom = posTop + elemHeight - viewport.height - viewportScroll + tipAdjust,
 							offset = myHeight - (my.precedance === 'y' || my.x === my.y ? atHeight : 0),
 							isCenter = my.y === 'center';
 
 						// Optional 'shift' style repositioning
-						if(readjust.vertical === 'shift') {
+						if(isShift) {
 							tipAdjust = tip && tip.precedance === 'x' ? tipHeight : 0;
-							offset = (my.y === 'top' ? myHeight : -myHeight) - tipAdjust;
+							offset = (my.y === 'top' ? 1 : -1) * myHeight - tipAdjust;
 
 							// Adjust position but keep it within viewport dimensions
 							position.top += overflowTop > 0 ? overflowTop : overflowBottom > 0 ? -overflowBottom : 0;
@@ -1200,7 +1202,7 @@ function QTip(target, options, id, attr)
 					width: viewport[ (viewport[0] === window ? 'w' : 'outerW') + 'idth' ](),
 					scrollLeft: viewport.scrollLeft(),
 					scrollTop: viewport.scrollTop(),
-					offset: viewport.offset() || { left:0, top: 0 }
+					offset: viewport.offset() || { left: 0, top: 0 }
 				};
 
 				// Adjust position based onviewport and adjustment options
