d5f9b47c9903023b013948a90f4d201abba0fb27
Craig Michael Thompson <craig@craigsworks.com>
2011-06-14 15:10:33 +0100

    Fixed tip issue

--- dist/jquery.qtip.basic.js
+++ dist/jquery.qtip.basic.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Wed Jun 8 00:02:52 2011 +0100
+* Date: Tue Jun 14 14:43:01 2011 +0100
 */
 
 /*jslint browser: true, onevar: true, undef: true, nomen: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
