516fd6b03252b3080ab26041d121cbc6c6ffbca0
Craig Michael Thompson <craig@craigsworks.com>
2011-08-27 16:20:38 +0100

    Added missing hooks

--- dist/jquery.qtip.basic.js
+++ dist/jquery.qtip.basic.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Sat Aug 27 16:05:30 2011 +0100
+* Date: Sat Aug 27 16:18:00 2011 +0100
 */
 
 /*jslint browser: true, onevar: true, undef: true, nomen: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
