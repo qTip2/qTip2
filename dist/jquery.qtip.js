8e997d7ddc1b1801bc57a67bd44c723d130a3104
Leigh Caplan <lcaplan@onehub.com>
2011-08-28 14:09:00 -0700

    Re-built with new build script

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Sat Aug 27 17:34:23 2011 +0100
+* Date: Sun Aug 28 14:05:44 2011 -0700
 */
 
 /*jslint browser: true, onevar: true, undef: true, nomen: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
