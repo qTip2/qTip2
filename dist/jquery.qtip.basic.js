d358b00c0c92ba555b9d6a69328fc0e365fed990
Craig Michael Thompson <craig@craigsworks.com>
2012-03-01 16:12:41 +0000

    Added valid package.json f or new jQuery plugins site

--- dist/jquery.qtip.basic.js
+++ dist/jquery.qtip.basic.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Mon Feb 27 02:13:34 2012 +0000
+* Date: Mon Feb 27 02:18:05 2012 +0000
 */
 
 /*jslint browser: true, onevar: true, undef: true, nomen: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
