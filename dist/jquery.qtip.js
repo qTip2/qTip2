1e7b27ab7b4d674d5befcea7079077eaa737059f
Craig Michael Thompson <craig@craigsworks.com>
2012-05-13 16:05:55 +0100

    Fix inconsistent border-radius on extra styles and fix bootsrap border-radius on content

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -9,7 +9,7 @@
 *   http://en.wikipedia.org/wiki/MIT_License
 *   http://en.wikipedia.org/wiki/GNU_General_Public_License
 *
-* Date: Sun May 13 07:55:33 2012 -0700
+* Date: Sun May 13 16:01:37 2012 +0100
 */
 
 /*jslint browser: true, onevar: true, undef: true, nomen: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
