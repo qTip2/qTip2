488e2fb00681d59f69faaa8f1004a4e8aa762974
Craig Michael Thompson <craig@craigsworks.com>
2012-05-29 17:23:05 +0100

    Fix iOS 4.3.3 and above positioning issues
    4.3.3 and above have a fix for the fixed positioning bug that was being
    accounted for by qTip2 in those versions. This has now been removed for
    those versions and the iOS plugin updated to be able to detect sub-sub
    versions i.e. 4.3.3 is now detected as a float 4.33 for easier checking.

--- src/core.js
+++ src/core.js
@@ -1279,8 +1279,11 @@ function QTip(target, options, id, attr)
 					position = position.offset;
 				}
 
-				// Adjust for position.fixed tooltips (and also iOS scroll bug in v3.2 - v4.0)
-				if((PLUGINS.iOS < 4.1 && PLUGINS.iOS > 3.1) || PLUGINS.iOS == 4.3 || (!PLUGINS.iOS && fixed)) {
+				// Adjust for position.fixed tooltips (and also iOS scroll bug in v3.2-4.0 & v4.3-4.3.2)
+				if((PLUGINS.iOS > 3.1 && PLUGINS.iOS < 4.1) || 
+					(PLUGINS.iOS >= 4.3 && PLUGINS.iOS < 4.33) || 
+					(!PLUGINS.iOS && fixed)
+				){
 					win = $(window);
 					position.left -= win.scrollLeft();
 					position.top -= win.scrollTop();
@@ -1764,9 +1767,9 @@ PLUGINS = QTIP.plugins = {
 	/*
 	 * iOS 3.2 - 4.0 scroll fix detection used in offset() function.
 	 */
-	iOS: parseFloat(
-		('' + (/CPU.*OS ([0-9_]{1,3})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
-			.replace('undefined', '3_2').replace('_','.')
+	iOS: parseFloat( 
+		('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
+		.replace('undefined', '3_2').replace('_', '.').replace('_', '')
 	) || FALSE,
 
 	/*
