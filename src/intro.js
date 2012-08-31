e16d9f2266a7c49ad164dd0d8269509354cab4bb
Tim Tucker <tim@timtucker.com>
2012-08-31 13:06:04 -0300

    Update src/intro.js
    Cleaner syntax

--- src/intro.js
+++ src/intro.js
@@ -1,11 +1,9 @@
 /*jslint browser: true, onevar: true, undef: true, nomen: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
 /*global window: false, jQuery: false, console: false, define: false */
 
-(function( window, document, undefined ) {
-"use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
-
 // Uses AMD or browser globals to create a jQuery plugin.
-(function(factory) {
+(function( factory, window, document, undefined ) {
+	"use strict";
 	if(typeof define === 'function' && define.amd) {
 		define(['jquery'], factory);
 	}
@@ -14,6 +12,8 @@
 	}
 }
 (function($) {
+	"use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
+	
 	// Munge the primitives - Paul Irish tip
 	var TRUE = true,
 		FALSE = false,
