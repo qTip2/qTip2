141f50b47d36a95c533b80d834fec2031314150e
Craig Michael Thompson <craig@craigsworks.com>
2012-10-15 13:37:08 +0100

    Add touchstart to unfocus binding for better touchscreen support on iOS and Android etc.

--- src/core.js
+++ src/core.js
@@ -468,7 +468,7 @@ function QTip(target, options, id, attr)
 
 		// Hide tooltip on document mousedown if unfocus events are enabled
 		if(('' + options.hide.event).indexOf('unfocus') > -1) {
-			posOptions.container.closest('html').bind('mousedown'+namespace, function(event) {
+			posOptions.container.closest('html').bind('mousedown'+namespace+' touchstart'+namesace, function(event) {
 				var elem = $(event.target),
 					enabled = self.rendered && !tooltip.hasClass(disabled) && tooltip[0].offsetWidth > 0,
 					isAncestor = elem.parents(selector).filter(tooltip[0]).length > 0;
