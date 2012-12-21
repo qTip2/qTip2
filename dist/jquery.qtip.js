7bcf9afbcda37b9319ffe8045cf75c2faaf1d982
Craig Michael Thompson <craig@craigsworks.com>
2012-12-21 17:13:14 +0000

    Official v2.0

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -1,4 +1,4 @@
-/*! qTip2 - Pretty powerful tooltips - v2.0.0pre - 2012-12-21
+/*! qTip2 - Pretty powerful tooltips - v2.0.0 - 2012-12-21
 * http://craigsworks.com/projects/qtip2/
 * Copyright (c) 2012 Craig Michael Thompson; Licensed MIT, GPL */
 
@@ -1726,7 +1726,7 @@ if(!$.ui) {
 }
 
 // Set global qTip properties
-QTIP.version = '2.0.0pre-nightly-9c40f78456';
+QTIP.version = '2.0.0-nightly-15f5c6bc20';
 QTIP.nextid = 0;
 QTIP.inactiveEvents = 'click dblclick mousedown mouseup mousemove mouseleave mouseenter'.split(' ');
 QTIP.zindex = 15000;
