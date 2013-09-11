59888cb96351336b51c061372ac97ec5f6550d9d
Craig Michael Thompson <craig@craigsworks.com>
2013-09-11 10:01:08 +0100

    Remove odd comments on use strict statement

--- src/core/intro.js
+++ src/core/intro.js
@@ -15,6 +15,5 @@
 	}
 }
 (function($) {
-	/* This currently causes issues with Safari 6, so for it's disabled */
-	"use strict"; // (Dis)able ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
+	"use strict"; // Enable ECMAScript "strict" operation for this function. See more: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
 
