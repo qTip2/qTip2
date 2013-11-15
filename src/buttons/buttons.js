76664960dd758999d5baa5e5989449f79cb0bb4c
polo <octav@behigh.es>
2013-11-15 12:33:03 +0100

    Refactoring + Bug Fixes

--- src/buttons/buttons.js
+++ src/buttons/buttons.js
@@ -1,101 +1,102 @@
 // Buttons bar
+var hasButtonBarClass = 'qtip-has-buttonbar';
+
 function ButtonsBar(api) {
-	var self = this,
-			buttons = api.options.content.buttons,
-			classes = api.options.style.buttons && api.options.style.buttons.classes,
-			elements = api.elements,
-			tooltip = elements.tooltip,
-			tooltipClass = 'qtip-has-buttonbar';
+	this.qtip = api;
+	this.classes = api.options.style.buttons && api.options.style.buttons.classes;
+	this.init(api.options.content.buttons);
+}
 
-	api.checks.buttons = {
-		'^content.buttons$': function () {
-			buttons = api.options.content.buttons;
-			self.init();
-		}
-	};
+$.extend(ButtonsBar.prototype, {
+	init: function (buttons) {
+		var elements = this.qtip.elements;
 
-	$.extend(self, {
-		init: function () {
-			if (!buttons) {
-				if (elements.buttonbar) {
-					elements.buttonbar.remove();
-					delete elements.buttonbar;
-					tooltip.removeClass(tooltipClass);
-				}
-				return self;
+		if (!buttons) {
+			if (elements.buttonbar) {
+				elements.buttonbar.remove();
+				delete elements.buttonbar;
+				elements.tooltip.removeClass(hasButtonBarClass);
 			}
-
-			if (!elements.buttonbar) {
-				elements.buttonbar = $('<div class="qtip-buttonbar"></div>').appendTo(tooltip);
-			}
-			tooltip.addClass(tooltipClass);
-			return self.set(buttons);
-		},
-		add: function (button) {
-			if (!elements.buttons) {
-				return self.set([button]);
-			}
-
-			var btn;
-
-			if (button.jquery) {
-				btn = button;
-			} else {
-				btn = $('<button>' + button.text + '</button>');
-				(classes && (typeof button.def == 'undefined' || button.def) && btn.addClass(classes));
-				(button.classes && btn.addClass(button.classes));
-				(button['attr'] && btn.attr(button['attr']));
-				(button['prop'] && btn.attr(button['prop']));
-				if (button.action) {
-
-					var action = button.action;
-					if (!action || typeof action == 'string') {
-						switch (action)
-						{
-							case 'close':
-							case 'hide':
-								action = function(e, api) {api.hide()};
-								break;
-							case 'cancel':
-							case 'destroy':
-								action = function(e, api) {api.destroy()};
-								break;
-							case 'submit':
-							case 'reset':
-								action = (function(method) {
-									return function(e, api) {
-										api.elements.content.find('form')[0][method]();
-									}
-								}(action));
-								break;
-							default:
-								action = function() {};
-						}
-					}
-					btn.bind('click', function (e) {
-						action.apply(btn, [e, api]);
-					});
-				}
-			}
-			btn.appendTo(elements.buttons);
-
 			return self;
-		},
-		set: function (buttons) {
-			(elements.buttons && elements.buttons.remove());
-			if (elements.buttonbar && typeof buttons === 'object' && !$.isEmptyObject(buttons)) {
-				elements.buttons = $('<div class="qtip-buttonbar-buttons"></div>').appendTo(elements.buttonbar);
-				$.each(buttons, function () {
-					self.add(this);
+		}
+
+		if (!elements.buttonbar) {
+			elements.buttonbar = $('<div class="qtip-buttonbar"></div>').appendTo(elements.tooltip);
+		}
+		elements.tooltip.addClass(hasButtonBarClass);
+		return this.set(buttons);
+	},
+	add: function (button) {
+		var elements = this.qtip.elements,
+			api = this.qtip;
+
+		if (!elements.buttons) {
+			return this.set([button]);
+		}
+
+		var btn;
+
+		if (button.jquery) {
+			btn = button;
+		} else {
+			btn = $('<button>' + button.text + '</button>');
+			(this.classes && (typeof button.def == 'undefined' || button.def) && btn.addClass(this.classes));
+			(button.classes && btn.addClass(button.classes));
+			(button['attr'] && btn.attr(button['attr']));
+			(button['prop'] && btn.attr(button['prop']));
+			if (button.action) {
+
+				var action = button.action;
+				if (typeof action == 'string') {
+					switch (action) {
+						case 'close':
+						case 'hide':
+							action = function (e, api) {
+								api.hide()
+							};
+							break;
+						case 'cancel':
+						case 'destroy':
+							action = function (e, api) {
+								api.destroy()
+							};
+							break;
+						case 'submit':
+						case 'reset':
+							action = (function (method) {
+								return function (e, api) {
+									api.elements.content.find('form')[0][method]();
+								}
+							}(action));
+							break;
+						default:
+							action = function () {};
+					}
+				}
+				btn.bind('click', function (e) {
+					action.apply(btn, [e, api]);
 				});
 			}
-
-			return self;
 		}
-	});
+		btn.appendTo(elements.buttons);
 
-	self.init();
-}
+		return self;
+	},
+	set: function (buttons) {
+		var elements = this.qtip.elements,
+			self = this;
+
+		(elements.buttons && elements.buttons.remove());
+		if (elements.buttonbar && typeof buttons === 'object' && !$.isEmptyObject(buttons)) {
+			elements.buttons = $('<div class="qtip-buttonbar-buttons"></div>').appendTo(elements.buttonbar);
+			$.each(buttons, function () {
+				self.add(this);
+			});
+		}
+
+		return this;
+	}
+});
 
 
 PLUGINS.buttons = function (api) {
@@ -103,6 +104,12 @@ PLUGINS.buttons = function (api) {
 };
 
 PLUGINS.buttons.initialize = 'render';
+
+CHECKS.buttons = {
+	'^content.buttons$': function () {
+		this.init(this.qtip.options.content.buttons);
+	}
+};
 
 // Extend original qTip defaults
 $.extend(TRUE, QTIP.defaults, {
