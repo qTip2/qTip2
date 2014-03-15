4dcf1b0e9066c4e40e632e98dcd64b9ad487d70c
Craig Michael Thompson <craig@craigsworks.com>
2014-03-15 13:02:17 +0000

    Remove dist/ directory in sourceMap files. Fixes #662

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,10 +147,28 @@ module.exports = function(grunt) {
 		},
 
 		replace: {
+			// Replacement variables are set up in `init` task
 			dist: {
 				files: [
 					 { expand: true, flatten: true, src: ['<%=dirs.dist%>/**/*'], dest: '<%=dirs.dist%>/' }
 				]
+			},
+
+			// Fix sourceMaps on remote CDNJS/jsDelivr services by removing dist/ in source map
+			sourcemap: {
+				files: [
+					 { expand: true, flatten: true, src: ['<%=dirs.dist%>/**/*.map'], dest: '<%=dirs.dist%>/' }
+				],
+				options: {
+					patterns: [
+						{
+							match: /dist\//g,
+							replacement: function() {
+								return grunt.config('pkg.type') === 'stable' ? '' : 'dist/';
+							}
+						}
+					]
+				}
 			}
 		},
 
