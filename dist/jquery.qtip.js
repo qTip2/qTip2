65edb14b9a942aebe07b315ad7ceba1f35b6c0e1
Craig Michael Thompson <craig@craigsworks.com>
2015-12-08 19:07:54 +0000

    Recompile distributable files

--- dist/jquery.qtip.js
+++ dist/jquery.qtip.js
@@ -1,12 +1,12 @@
 /*
- * qTip2 - Pretty powerful tooltips - v3.0.0-3-g
+ * qTip2 - Pretty powerful tooltips - v3.0.0-4-g
  * http://qtip2.com
  *
  * Copyright (c) 2015 
  * Released under the MIT licenses
  * http://jquery.org/license
  *
- * Date: Tue Dec 8 2015 07:05 GMT+0000
+ * Date: Tue Dec 8 2015 07:07 GMT+0000
  * Plugins: tips modal viewport svg imagemap ie6
  * Styles: core basic css3
  */
@@ -1928,7 +1928,7 @@ if(!$.ui) {
 	};
 }
 ;// qTip version
-QTIP.version = '3.0.0-3-g';
+QTIP.version = '3.0.0-4-g';
 
 // Base ID for all qTips
 QTIP.nextid = 0;
